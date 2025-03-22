98fe499e2f1f7fd7af569e2ef4ffa9f7c960a83f
Sirapob Chonphanarak <76686829+Sirapobchon@users.noreply.github.com>
2025-03-22 18:50:33 +0700

    Minior Update

--- FreeRTOS/FreeRTOSConfig.h
+++ FreeRTOS/FreeRTOSConfig.h
@@ -90,7 +90,7 @@
 #define configUSE_IDLE_HOOK			1
 #define configUSE_TICK_HOOK			0
 #define configCPU_CLOCK_HZ			( ( unsigned long ) F_CPU)
-#define configTICK_RATE_HZ			( ( TickType_t ) 1000 )
+#define configTICK_RATE_HZ			( ( TickType_t ) 2000 )
 #define configMAX_PRIORITIES		( 4 )
 #define configMINIMAL_STACK_SIZE	( ( unsigned short ) 85 )
 #define configTOTAL_HEAP_SIZE		( ( 1200 ) )
